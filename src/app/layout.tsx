3381c1a77a627f2f2ede1dbe938d6ea833b8cddd
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-05-28 23:43:10 +0900

    init: 사이드 내비게이션 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SideNav from "@/components/side-nav/SideNav";
 
 export const metadata: Metadata = {
   title: "Pebble",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SideNav />
+        {children}
+      </body>
     </html>
   );
 }
